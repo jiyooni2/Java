8d96cf8dd14a8b19a638f21a627a8f02a97de404
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-05-17 17:56:34 +0900

    variable

---
+++ src/variable.java
@@ -0,0 +1,43 @@
+public class variable {
+   public static void main(String[] args) {
+      //without newline
+      System.out.print(4);
+      //with new line
+      System.out.println(4);
+
+      int x = 200, y = 15;
+      System.out.println(x);
+      System.out.println(x / y);
+
+      long a = 200L;
+      long b = 200;
+      float f = 1.25F;
+      double d = 2.5;
+
+      char ch = 'a';
+      String str2 = new String("java");
+
+      String str = "Java";
+      System.out.println(str);
+      System.out.println(str2);
+
+      int age = 20;
+      System.out.printf("%d%n", age);
+
+      String temp = "3";
+
+      //string to char
+      System.out.println(temp.charAt(0));
+      //int to char
+      System.out.println((char) (3 + '0'));
+      //char to int
+      System.out.println('3' - '0');
+      //number to string
+      System.out.println(3 + "");
+      //string to number
+      System.out.println(Integer.parseInt("4"));
+      System.out.println(Double.parseDouble("3.14"));
+      //char to string
+      System.out.println('3' + "");
+   }
+}

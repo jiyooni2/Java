0d21bc8df4a669fae16bc58a24c6967c42e98151
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-12-15 20:51:03 +0900

    HashSet, Collections

---
+++ src/Ch11_Comparator.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Ch11_Comparator {
+  public static void main(String[] args) {
+    String[] strArr = {"cat", "Dog", "lion", "tiger"};
+
+    Arrays.sort(strArr); // String의 Comparable에 따라 정렬
+    System.out.println("strArr = " + Arrays.toString(strArr));
+
+    Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER); //대소문자 구분 X
+    System.out.println("strArr = " + Arrays.toString(strArr));
+
+    Arrays.sort(strArr, new Descending()); // Comparator를 구현한 Descending에 따라 정렬
+    System.out.println("strArr = " + Arrays.toString(strArr));
+  }
+}
+
+class Descending implements Comparator {
+  public int compare(Object o1, Object o2) {
+    if (o1 instanceof Comparable && o2 instanceof Comparable) {
+      Comparable c1 = (Comparable) o1;
+      Comparable c2 = (Comparable) o2;
+      return c2.compareTo(c1);
+    }
+    return -1;
+  }
+}
+

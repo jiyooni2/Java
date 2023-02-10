161cf65b3ac3d3080c7b848118c85a5e56e1cf26
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2023-02-10 18:28:44 +0900

    Generic

---
+++ src/Ch12_Generic.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Product {
+}
+
+class Tv extends Product {
+}
+
+class Audio extends Product {
+}
+
+public class Ch12_Generic {
+
+    public static void main(String[] args) {
+        ArrayList<Product> productList = new ArrayList<Product>();
+        ArrayList<Tv> tvList = new ArrayList<Tv>();
+
+    }
+}
